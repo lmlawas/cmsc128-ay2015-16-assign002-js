c6b6c4a79c99700c270251bf5ee146564b83bfec
lmlawas1 <leenseylawas2007@gmail.com>
2016-03-07 15:25:25 +0800

    Improved documentation

--- script/bioinformatics_lib.js
+++ script/bioinformatics_lib.js
@@ -3,11 +3,14 @@
 	Dates Accomplished:	March 6, 7, 2016
 */
 
-function getHammingDistance(){
-	// get value from the form and store to variables
-	var str1 = document.forms["hammingDistance"]["str1"].value;
-	var str2 = document.forms["hammingDistance"]["str2"].value;
-
+function getHammingDistance(str1, str2){
+/*******************************************************************************
+	Given two strings str1 and str2 of same length (length must never be 0 or
+	negative!), the Hamming Distance of those two strings are the number of 
+	inversions per character need to transform str1 to str2 or vise-versa. 
+	Simply put, the Hamming Distance of two strings is the number of characters 
+	that differ in ith position from position 1 to strlen(str1).
+*******************************************************************************/
 		// if str1 is empty
 		if (str1 == null || str1 == "") {	       
 	       document.getElementById("hd").innerHTML =
@@ -49,58 +52,91 @@ function getHammingDistance(){
 		}		
 }// end of getHammingDistance()
 
-function countSubstrPattern(){
-	// get value from the form and store to variables
-	var o = document.forms["substringPattern"]["original"].value;
-	var p = document.forms["substringPattern"]["pattern"].value;
+function countSubstrPattern(original, pattern){	
+/*******************************************************************************
+	Given a string original and pattern, we will count the number of occurrence
+	of pattern in original.
+*******************************************************************************/	
 	var patt = /[^CGATU]/;
 
 	// check if original is greater than or equal to pattern
-	if(o.length<p.length){
+	if(original.length<pattern.length){
 		document.getElementById("sp").innerHTML = 
 		"Error! Strings are not equal!";		
 	}
 	// check if original is a valid nucloebase
-	else if(patt.test(o)==true){
+	else if(patt.test(original)==true){
 		document.getElementById("sp").innerHTML = 
 		"Error! Original is not a nucleobase!";	
 	}
 	// check if pattern is a valid nucloebase
-	else if(patt.test(p)==true){
+	else if(patt.test(pattern)==true){
 		document.getElementById("sp").innerHTML = 
 		"Error! Pattern is not a nucleobase!";
 	}
 	else{
 		var cnt=0;
 		var i;
-		for(i=0;i<=o.length-p.length;i++){
-			// if the substring of o matches string p			
-			if(o.substring(i, p.length+i) == p) cnt++;
+		for(i=0;i<=original.length-pattern.length;i++){
+			// if the substring of original matches string pattern
+			if(original.substring(i, pattern.length+i) == pattern) cnt++;
 		}
 		document.getElementById("sp").innerHTML = cnt;
 	}
 	
 }// end of countSubstrPattern()
 
-function isValidString(){
-	// get value from the form and store to variables
-	var str = document.forms["validString"]["str3"].value;
-	var a = document.forms["validString"]["alphabet"].value;
-	var patt = new RegExp("[^"+a+"]", "g");			
+function isValidString(str, alphabet){
+/*******************************************************************************
+	Given an alphabet string where all letters are assumed to be unique, this
+	function returns true if the string str is a valid string based on the 
+	letters of alphabet.
+*******************************************************************************/	
+	// get regular expression from alphabet
+	var patt = new RegExp("[^"+alphabet+"]", "g");			
 
+	// if pattern is true, display false
 	if(patt.test(str)) document.getElementById("vs").innerHTML = false;
 	else document.getElementById("vs").innerHTML = true;
 	
 }// end of isValidString()
 
 function getSkew(){
+/*******************************************************************************
+	Given a genome str of some length q (where q>0), it returns the number of
+	Gs minus the number of Cs in the first n nucleotides (q>=n). The value can 
+	be zero, negative or positive. The first position is one (1) not zero(0) 
+	as we typically associate with string implementations.
+*******************************************************************************/	
 
 }// end of getSkew()
 
 function getMaxSkewN(){
+/*******************************************************************************
+	Given a genome str of some length q (where q>0), it returns the maximum
+	value of the number of Gs minus the number of Cs in the first n nucleotides 
+	(q>=n). The value can be zero, negative or positive. The first position is 
+	one (1) not zero(0) as we typically associate with string implementations.
+*******************************************************************************/
 
 }// end of getMaxSkewN()
 
 function getMinSkewN(){
-
+/*******************************************************************************
+	Given a genome str of some length q (where q>0), it returns the minimum
+	value of the number of Gs minus the number of Cs in the first n nucleotides 
+	(q>=n). The value can be zero, negative or positive. The first position is 
+	one (1) not zero(0) as we typically associate with string implementations.
+*******************************************************************************/
 }// end of getMinSkewN()
+
+/*
+
+<script>
+function myFunction() {
+    return Math.PI;
+}
+
+document.getElementById("demo").innerHTML = myFunction();
+</script>
+*/
